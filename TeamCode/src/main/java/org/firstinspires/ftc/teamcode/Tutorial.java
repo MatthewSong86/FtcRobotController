62932f6141edf72813fa961d4d51f09c7660c991
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2021-09-10 20:57:13 -0700

    Update Tutorial.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tutorial.java
@@ -9,19 +9,6 @@ public class Tutorial extends LinearOpMode {
     public DcMotor motor1;
     public CRServo servo1;
     public void runOpMode() throws InterruptedException {
-        DcMotor motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        CRServo servo1 = hardwareMap.get(CRServo.class, "servo1");
-        telemetry.addData("Status", "Initalized");
-        telemetry.update();
-        waitForStart();
-
-        while(opModeIsActive()){
-            motor1.setPower(gamepad1.right_stick_y);
-            if(gamepad1.left_stick_y<0) servo1.setPower(0);
-            else if(gamepad1.left_stick_y>0) servo1.setPower(1);
-            else servo1.setPower(0.5);
-
-
-        }
+      
     }
 }
